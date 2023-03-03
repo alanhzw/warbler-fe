f9c5136f8453a31abb0d659ce7a72d66bc5bd0a0
warbler <1741847465@qq.com>
2023-03-03 10:57:26 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank"> warbler-js </a>
+    <a href="https://vuejs.org/" target="_blank"> warbler-cli </a>
+    <a href="https://vuejs.org/" target="_blank"> 个人中心 </a>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
