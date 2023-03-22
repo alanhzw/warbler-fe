c6e7fb6a34f2fa3aa10b68fd5103c94be6f646bf
warbler <1741847465@qq.com>
2023-03-22 14:55:11 +0800

    test: test eslint

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+// s
+</script>
+
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> warbler-js </a>
-    <a href="https://vuejs.org/" target="_blank"> warbler-cli </a>
-    <a href="https://vuejs.org/" target="_blank"> 个人中心 </a>
+    <a
+      href="https://vitejs.dev"
+      target="_blank"
+    >
+      <img
+        src="/vite.svg"
+        class="logo"
+        alt="Vite logo"
+      >
+    </a>
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+    >
+      <img
+        src="./assets/vue.svg"
+        class="logo vue"
+        alt="Vue logo"
+      >
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script setup lang="ts"></script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
 
-<style scoped></style>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
