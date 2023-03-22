e63790e15a807e59a995e33b5bcb08703aa29d28
warbler <1741847465@qq.com>
2023-03-22 15:13:30 +0800

    test: 测试husky

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-console */
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
 createApp(App).mount('#app');
-// sda
+
 // test
+if (true) {
+  console.log('!');
+}
