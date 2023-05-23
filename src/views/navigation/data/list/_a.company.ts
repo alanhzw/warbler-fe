8ef5722d148d6a9259c5407433df2d2f12e90cbe
jm032538 <zhiwei.han@baozun.com>
2023-05-23 09:12:22 +0800

    🚀 新增: 添加宝尊相关链接

---
+++ src/views/navigation/data/list/_a.company.ts
@@ -0,0 +1,47 @@
+import type { NavList } from '../index';
+
+const list: NavList = [
+  {
+    icon: 'https://wiki.baozun.com/s/-s7n0x/8401/8e8f74c7e2fea2df662fe8d0f5d79ff6ab016336/1/_/favicon.ico',
+    name: 'wiki-UNEX-前端组',
+    link: 'https://wiki.baozun.com/pages/viewpage.action?pageId=118358480',
+    iconErrorText: '',
+  },
+  {
+    icon: 'http://npmjs.baozun.com/favicon.png',
+    name: 'npm 宝尊',
+    link: 'http://npmjs.baozun.com/',
+    iconErrorText: '',
+  },
+  {
+    icon: 'https://bee.baozun.com/static/honeybee.ec09a0b5.svg',
+    name: '小蜜蜂运维平台',
+    link: 'https://bee.baozun.com/homepage',
+    iconErrorText: '',
+  },
+  {
+    icon: 'http://git.baozun.com/assets/favicon-7901bd695fb93edb07975966062049829afb56cf11511236e61bcf425070e36e.png',
+    name: 'GitLab',
+    link: 'http://git.baozun.com/',
+    iconErrorText: '',
+  },
+  {
+    icon: 'https://shopkeeper-plus-sit.baozun.com/favicon.ico',
+    name: 'pioneer 测试',
+    link: 'http://shopkeeper-plus-sit.baozun.com/',
+    iconErrorText: '',
+  },
+  {
+    icon: '',
+    name: 'light 测试',
+    link: 'https://light-sit.baozun.com/',
+    iconErrorText: 'L',
+  },
+];
+
+const listData = {
+  title: '宝尊电商',
+  list,
+};
+
+export default listData;
