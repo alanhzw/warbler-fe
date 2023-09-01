6e4964b417539c7101dbdcedf3654efcf5ffbef2
hanzhiwei <hanzhiwei@lixiang.com>
2023-09-01 17:18:10 +0800

    🚀 功能: 新增一些导航

--- dist/assets/index-e63b56f5.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as c,c as t,p as o,a as _,b as a}from"./index-ef783536.js";const d="/assets/wechat-86ade148.jpg";const n={},p=e=>(o("data-v-a54ad85a"),e=e(),_(),e),i={class:"contact"},r=p(()=>a("div",{class:"wechat"},[a("img",{src:d})],-1)),h=[r];function l(e,f){return c(),t("div",i,h)}const u=s(n,[["render",l],["__scopeId","data-v-a54ad85a"]]);export{u as default};
