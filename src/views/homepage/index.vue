d2a7eb64d2c0fca4092e0025ff8381bca310396c
hanzhiwei <hanzhiwei@lixiang.com>
2023-12-04 09:16:05 +0800

    🚀 功能: 首页添加备案号

--- src/views/homepage/index.vue
+++ src/views/homepage/index.vue
@@ -2,13 +2,14 @@
  * @Author: 一尾流莺
  * @Description:网站首页
  * @Date: 2023-03-24 14:13:01
- * @LastEditTime: 2023-03-30 15:18:32
+ * @LastEditTime: 2023-12-04 09:13:17
  * @FilePath: \warbler-fe\src\views\homepage\index.vue
 -->
 <template>
   <div class="homepage-view">
     <canvas id="canvas" class="canvas"></canvas>
     <typewriter></typewriter>
+    <div class="copyright" @click="gotoLink">辽ICP备2020012862号</div>
   </div>
 </template>
 
@@ -16,6 +17,10 @@
 import { onMounted } from 'vue';
 import typewriter from './components/typewriter.vue';
 import { renderCanvas } from './canvas';
+
+const gotoLink = () => {
+  window.open('https://beian.miit.gov.cn/');
+};
 
 onMounted(() => {
   renderCanvas();
@@ -25,5 +30,12 @@ onMounted(() => {
 <style lang="scss" scoped>
 canvas {
   display: block;
+}
+.copyright {
+  cursor: pointer;
+  position: fixed;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
