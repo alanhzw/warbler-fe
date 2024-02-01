a43f91bdbbad628f0d98bf90c2db3290d682affc
hanzhiwei <hanzhiwei@lixiang.com>
2024-02-01 17:00:56 +0800

    🚀 功能: 更新二维码

--- src/views/contact/index.vue
+++ src/views/contact/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="contact">
+    <div>
+      加群的小伙伴意外的多，群已经满200人了，不能扫码进群了，还请各位添加我微信，拉你们进群吧
+    </div>
+    <div style="margin-top: 20px">群内活跃的小伙伴很多，希望各位加群后保持活跃</div>
     <div class="wechat">
       <img src="@/assets/image/wechat.jpg" />
-      <img src="@/assets/image/group.jpg" />
+      <img src="@/assets/image/full.jpg" />
     </div>
   </div>
 </template>
@@ -14,6 +18,7 @@
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   .wechat img {
